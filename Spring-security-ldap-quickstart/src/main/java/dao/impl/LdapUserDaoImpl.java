6570060b7b4814f96c70a7b8703dc48902076fbe
Dreamltf <Dreamltf@Hachu10>
2014-05-03 23:42:37 +0800

    remove unnecessary code

--- Spring-security-ldap-quickstart/src/main/java/dao/impl/LdapUserDaoImpl.java
+++ Spring-security-ldap-quickstart/src/main/java/dao/impl/LdapUserDaoImpl.java
@@ -4,29 +4,23 @@ import java.util.List;
 
 import model.LdapUser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.query.LdapQuery;
 import org.springframework.ldap.query.LdapQueryBuilder;
+import org.springframework.stereotype.Repository;
 
 import dao.LdapUserDao;
 
+@Repository
 public class LdapUserDaoImpl implements LdapUserDao {
 
+	@Autowired
 	private LdapTemplate ldapTemplate;
+	@Value("${user_id_attr}")
 	private String userIdAttribute;
-
-	public LdapUserDaoImpl() {
-		super();
-	}
-
-	public void setLdapTemplate(LdapTemplate ldapTemplate) {
-		this.ldapTemplate = ldapTemplate;
-	}
-
-	public void setUserIdAttribute(String userIdAttribute) {
-		this.userIdAttribute = userIdAttribute;
-	}
 
 	@Override
 	public LdapUser findLdapUserById(String userId) {
