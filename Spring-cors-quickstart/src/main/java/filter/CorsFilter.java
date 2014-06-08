e48ce17c2a71caf98b77982a232b34dc76349892
Dreamltf <Dreamltf@Hachu10>
2014-06-08 18:06:46 +0800

    move config into init parameters

--- Spring-cors-quickstart/src/main/java/filter/CorsFilter.java
+++ Spring-cors-quickstart/src/main/java/filter/CorsFilter.java
@@ -8,10 +8,13 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class CorsFilter implements Filter {
+
+	private String accessControlAllowOrigin = "*";
+	private String accessControlAllowMethods = "POST, DELETE, GET, PUT, OPTIONS";
+	private String accessControlAllowHeaders = "Origin, X-Requested-With, Content-Type, Accept";
 
 	public CorsFilter() {
 		super();
@@ -19,24 +22,39 @@ public class CorsFilter implements Filter {
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
+		String accessControlAllowOrigin = filterConfig
+				.getInitParameter("Access-Control-Allow-Origin");
+		String accessControlAllowMethods = filterConfig
+				.getInitParameter("Access-Control-Allow-Methods");
+		String accessControlAllowHeaders = filterConfig
+				.getInitParameter("Access-Control-Allow-Headers");
+		if (accessControlAllowOrigin != null
+				&& accessControlAllowOrigin.length() > 0) {
+			this.accessControlAllowOrigin = accessControlAllowOrigin;
+		}
+		if (accessControlAllowMethods != null
+				&& accessControlAllowMethods.length() > 0) {
+			this.accessControlAllowMethods = accessControlAllowMethods;
+		}
+		if (accessControlAllowHeaders != null
+				&& accessControlAllowHeaders.length() > 0) {
+			this.accessControlAllowHeaders = accessControlAllowHeaders;
+		}
 	}
 
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res,
 			FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
-		HttpServletRequest request = (HttpServletRequest) req;
 
 		// cors header
-		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Origin",
+				accessControlAllowOrigin);
 		response.setHeader("Access-Control-Allow-Methods",
-				"POST, DELETE, GET, PUT, OPTIONS");
+				accessControlAllowMethods);
 		response.setHeader("Access-Control-Allow-Headers",
-				"Origin, X-Requested-With, Content-Type, Accept");
-		// response options method check
-		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-			return;
-		}
+				accessControlAllowHeaders);
+
 		chain.doFilter(req, res);
 	}
 
